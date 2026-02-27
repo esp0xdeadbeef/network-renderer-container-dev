c6cfeaa33e8bc093e8649fb7e05fdb513c892267
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-02-28 00:49:34 +0100

    compling fails, ERROR: Missing required solver field: routing assumptions, working on it now

--- run-clab-generator.sh
+++ run-clab-generator.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-nix run .#generate-clab-config ../network-compiler/examples/single-wan/inputs.nix
+#nix run .#generate-clab-config ../network-compiler/examples/single-wan/inputs.nix
+nix run .#generate-clab-config ../network-compiler/examples/multi-wan/inputs.nix
