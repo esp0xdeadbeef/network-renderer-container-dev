5e337f8dd5837c4122530e231f21aff63ce0757f
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-06 23:51:40 +0100

    update start-vm, just cleaning up

--- start-vm.sh
+++ start-vm.sh
@@ -1,22 +1,10 @@
-# ./start-vm.sh
 #!/usr/bin/env bash
 set -euo pipefail
 
 ./run-clab-generator.sh
+
 touch ./nixos.qcow2
 rm -f ./nixos.qcow2
-
-export QEMU_NET_OPTS="hostfwd=tcp::2222-:22"
-echo "ssh -o 'StrictHostKeyChecking no' -p2222 root@localhost # to connect to the vm."
-
-FLAKE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-set -euo pipefail
-
-export QEMU_NET_OPTS="hostfwd=tcp::2222-:22"
-echo "ssh -o 'StrictHostKeyChecking no' -p2222 root@localhost # to connect to the vm."
-
-#!/usr/bin/env bash
-set -euo pipefail
 
 export QEMU_NET_OPTS="hostfwd=tcp::2222-:22"
 echo "ssh -o 'StrictHostKeyChecking no' -p2222 root@localhost # to connect to the vm."
@@ -30,7 +18,6 @@ BRIDGES_FILE="${FLAKE_DIR}/vm-bridges-generated.nix"
 
 if [ ! -f "${SOLVER_JSON}" ] || [ ! -f "${BRIDGES_FILE}" ]; then
   echo "[*] Generating solver JSON, topology and bridges..."
-  #nix run .#generate-clab-config "${SOLVER_INPUT}" "${TOPO_FILE}" "${BRIDGES_FILE}"
   ./run-clab-generator.sh
 fi
 
