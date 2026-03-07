7e9110b6449fa932e6caf6e54daa9753164238eb
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-08 00:00:08 +0100

    other inputs

--- vm-bridges-generated.nix
+++ vm-bridges-generated.nix
@@ -2,13 +2,10 @@
 {
   bridges = [
     "br-1ae349e25379"
-    "br-1dec167e9f83"
     "br-4743c4e638ef"
     "br-5980f0dc953e"
     "br-6fff74cec0ec"
     "br-8240a4b630f9"
-    "br-8897a6fa9b88"
-    "br-9137ba2a7571"
     "br-93d709323a57"
     "br-d915c87afd65"
     "br-e035268ceff9"
